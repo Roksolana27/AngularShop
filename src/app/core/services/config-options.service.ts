94ae8206e63ea3fe89deeb60936dd7ec8da79553
KYIV\Roksolana_Mykhalenyc <roksolana_mykhalenych@epam.com>
2018-03-30 15:38:48 +0300

    Generator created

--- src/app/core/services/config-options.service.ts
+++ src/app/core/services/config-options.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigOptionsService {
-  private id: string = '';
-  private login: string = '';
-  private email: string = '';
-  private password: string = '';
 
   constructor() { }
 
